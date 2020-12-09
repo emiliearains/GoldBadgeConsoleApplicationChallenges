0667a06f7b00d089826d47237dbaa2ff178b1917
Emilie Rains <emiliearains@gmail.com>
2020-12-08 22:10:00 -0500

    Add project files.

---
+++ ChallengeOneClassLibrary/Menu.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChallengeOneClassLibrary
+{
+    public class Menu
+    {
+        public int MealNumber { get; set; }
+        public string MealName { get; set; }
+        public string MealDescription { get; set; }
+        public List<string> Ingredients { get; set; }
+        public decimal MealPrice { get; set; }
+
+        public Menu(int mealNumber, string mealName, string mealDescription, List<string> ingredients, decimal mealPrice)
+        {
+            MealNumber = mealNumber;
+            MealName = mealName;
+            MealDescription = mealDescription;
+            Ingredients = ingredients;
+            MealPrice = mealPrice;
+        
+        } 
+    }
+}
